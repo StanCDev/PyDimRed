708cf7db78e6a26b7af8c9c86092863dfbb2efd2
StanCDev <stancastellana@icloud.com>
2024-08-29 14:01:30 +0200

    Clean: pypi setting

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PyDimRed",
-    version="0.0.2",
+    version="0.0.3",
     author="StanCDev",
     author_email="stancastellana@icloud.com",
     description="Dimensionality reduction library",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/StanCDev/PyDimRed",
-    download_url='https://github.com/StanCDev/PyDimRed/archive/refs/tags/v_02.tar.gz',
+    download_url='https://github.com/StanCDev/PyDimRed/archive/refs/tags/v_03.tar.gz',
     packages= setuptools.find_packages(),
     install_requires= 
     [
